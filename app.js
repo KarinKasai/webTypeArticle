33de0f93e76b807c81efa892d83dd3368234ed61
Karin Kasi <karinrin22@gmail.com>
2021-02-27 22:46:10 -0500

    repost

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 
+// nav bar scroll effect
+
+
+window.addEventListener('scroll', function () {
+    let navbar = document.querySelector('nav');
+    let windowPosition = window.scrollY > 0;
+    navbar.classList.toggle('scrolling-active', windowPosition);
+})
+
+
+
+
+
+
+
 
 // parallax effect
 
@@ -6,7 +21,7 @@ window.addEventListener('scroll', function() {
     const parallax_margella = document.querySelector('.parallax_1');
     let scrollPosition = window.pageYOffset;
 
-    parallax_margella.style.transform = 'translateX(' + scrollPosition / 1.2 + 'px)';
+    parallax_margella.style.transform = 'translateX(' + scrollPosition / 1.1 + 'px)';
 
 
 
@@ -16,7 +31,7 @@ window.addEventListener('scroll', function() {
     const parallax_prada = document.querySelector('.parallax_2');
     let scrollPosition = window.pageYOffset;
 
-    parallax_prada.style.transform = 'translateX(' + scrollPosition / -1.2 + 'px)';
+    parallax_prada.style.transform = 'translateX(' + scrollPosition / -1.1 + 'px)';
 
 
 
